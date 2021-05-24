a26fe4f6a4b28a13bc8bad3b17ef31f16224b02f
favdev111 <webdev0429@outlook.com>
2021-05-24 10:50:00 +0300

    chore:

--- pages/pitch.js
+++ pages/pitch.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import Header from "@c/Header";
-import PitchBlock from "@c/PitchBlock";
 
+import React from "react";
+import Header from "@layout/header";
+import PitchBlock from "@page/pitch/PitchBlock";
 
 const PitchPage = () => {
-
-    return(
-        <div className="container">
-            <Header/>
-            <main>
-               <PitchBlock />
-            </main>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <Header />
+      <main>
+        <PitchBlock />
+      </main>
+    </div>
+  );
+};
 
 export default PitchPage;
