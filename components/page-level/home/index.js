bde5a00d1c9d6e4c062e7d5235fae647cde7e4f1
favdev111 <webdev0429@outlook.com>
2021-06-01 13:05:16 +0300

    feat: tag active style, events

--- components/page-level/home/index.js
+++ components/page-level/home/index.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 import CommonSearch from "@sub/search";
 import Tag from "./tag";
 
 function Home() {
+  const [activeTag, setActiveTag] = useState(0);
+
   return (
     <div className={styles.homePage}>
       <CommonSearch />
       <h1> Videos</h1>
       <div className={styles.tagContent}>
         {["All Sports", "Football", "Cricket", "Netball", "Boxing"].map(
-          (tag) => (
-            <Tag> {tag} </Tag>
+          (tag, index) => (
+            <Tag
+              activeTag={activeTag}
+              onClick={() => setActiveTag(index)}
+              index={index}
+              key={tag + index}
+            >
+              {tag}
+            </Tag>
           )
         )}
       </div>
--- components/page-level/home/tag/index.js
+++ components/page-level/home/tag/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./index.module.css";
+import cn from "classnames";
 
-function Tag({ children }) {
-  return <div className={styles.tag}>{children}</div>;
+function Tag({ children, activeTag, index, onClick }) {
+  return (
+    <div
+      onClick={onClick}
+      className={cn(styles.tag, activeTag == index && styles.active)}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default Tag;
