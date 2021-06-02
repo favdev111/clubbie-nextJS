9e4517918a76edd94439a4c109f8a6163fe6f98b
favdev111 <webdev0429@outlook.com>
2021-06-02 20:25:25 +0500

    chore: responsive profile edit page created

---
+++ components/page-level/profile-self/edit/index.js
@@ -0,0 +1,96 @@
+import React from "react";
+import styles from "./profileedit.module.css";
+import cn from "classnames";
+import Chip from "@sub/Chip";
+import TemplateInput from "@sub/input";
+import DirectedButton from "@sub/button-directed";
+
+function ProfileEdit({ data }) {
+  const removeClub = () => {};
+
+  return (
+    <>
+      <div className={styles.profilePlayerBodyContent}>
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <p>Full Name</p>
+          <TemplateInput
+            type="text"
+            name="fullName"
+            required
+            value={data.name}
+          />
+        </div>
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <p>Title</p>
+          <TemplateInput type="text" name="role" required value={data.role} />
+        </div>
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <p>Email</p>
+          <TemplateInput type="text" name="email" required value={data.email} />
+        </div>
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <p>Telephone</p>
+          <TemplateInput
+            type="text"
+            name="telephone"
+            required
+            value={data.telephone}
+          />
+        </div>
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <p>Address</p>
+          <TemplateInput
+            type="text"
+            name="address"
+            required
+            value={data.address}
+          />
+        </div>
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <p>City</p>
+          <TemplateInput type="text" name="city" required value={data.city} />
+        </div>
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <p>Country</p>
+          <TemplateInput
+            type="text"
+            name="country"
+            required
+            value={data.country}
+          />
+        </div>
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <p>Postal Code</p>
+          <TemplateInput
+            type="text"
+            name="postalCode"
+            required
+            value={data.postalCode}
+          />
+        </div>
+      </div>
+      <div className={styles.profilePlayerClubChips}>
+        <div className={cn(styles.span3, styles.profilePlayerBodyContentItem)}>
+          <p>Clubs</p>
+        </div>{" "}
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <Chip text="Shottery United" onCloseClick={removeClub} />
+        </div>
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <Chip text="Shottery United" onCloseClick={removeClub} />
+        </div>
+        <div className={cn(styles.span1, styles.profilePlayerBodyContentItem)}>
+          <Chip
+            component={
+              <DirectedButton direction="forward">
+                <span className={styles.btnChipPadding}>Join Club</span>
+              </DirectedButton>
+            }
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProfileEdit;
---
+++ pages/profile-self-edit.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Layout from "@layout";
+import Seo from "@layout/seo";
+import ProfileSelf from "@page/profile-self";
+
+function ProfilePage({ person }) {
+  return (
+    <Layout>
+      <Seo title="Edit Profile" desc="Edit Your Public Profile on Clubbie" />
+      <ProfileSelf person={person} editMode />
+    </Layout>
+  );
+}
+
+export default ProfilePage;
+
+export const getStaticProps = async () => {
+  const person = {
+    name: "Roger Steward",
+    image: "/assets/profile-avatar.png",
+    role: "Footballer",
+    email: "roger@gmail.com",
+    telephone: "+44 7500 555 555",
+    city: "Statford-Upon-Avon",
+    country: "United Kingdom",
+    postCode: "CV37 4HB",
+    bio:
+      "Etiam convallis elementum sapien, a aliquam turpis aliquam vitae. Praesent sollicitudin felis vel mi facilisis posuere. Nulla ultrices facilisis justo, non varius nisl semper vel.",
+  };
+  return {
+    props: {
+      person,
+    },
+  };
+};
