9f31cf6757238be3350eb2c3f2605cc897356bfc
favdev111 <webdev0429@outlook.com>
2021-06-12 18:18:41 +0300

    feat: Events -- Date Table

--- components/page-level/teamhub-dashboard/tabs/events/index.js
+++ components/page-level/teamhub-dashboard/tabs/events/index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./index.module.css";
+import EditIcon from "@svg/edit.js";
+import DateTable from "./date-table";
 
 function Events() {
+  const date = new Date();
+  const month = date.getMonth();
+  const [selectedMonth, setSelected] = useState(month);
   return (
-    <div>
-      <h1> Events</h1>
-      <h3> My team </h3>
+    <div className={styles.eventContent}>
+      <div className={styles.event}>
+        <div className={styles.eventHeader}>
+          <h1> Events</h1>
+
+          {/* Drafts, Add event etc */}
+          <div className={styles.draft}>
+            <EditIcon />
+            <p>2 in Draft </p>
+          </div>
+        </div>
+
+        {/* Date */}
+        <DateTable setSelected={setSelected} selected={selectedMonth} />
+      </div>
     </div>
   );
 }
