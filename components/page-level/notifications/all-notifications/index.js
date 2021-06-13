2386cfa33165491c8b1e36cb343a5bde6435b2f5
favdev111 <webdev0429@outlook.com>
2021-06-13 14:48:31 +0300

    feat: Notification Page

---
+++ components/page-level/notifications/all-notifications/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import NotificationRow from "@sub/notification-list/row";
+import NotificationCard from "@sub/notification-list/card";
+
+function AllNotifications() {
+  const notifications = [
+    {
+      src: "./assets/team1.png",
+      title: "Shottery Mens First Team",
+      desc: "Line up is now up!",
+      hour: "10:35",
+    },
+    {
+      src: "./assets/team1.png",
+      title: "Shottery Mens First Team",
+      desc: "Line up is now up!",
+      hour: "10:35",
+    },
+    {
+      src: "./assets/team1.png",
+      title: "Shottery Mens First Team",
+      desc: "Line up is now up!",
+      hour: "10:35",
+    },
+    {
+      src: "./assets/team1.png",
+      title: "Shottery Mens First Team",
+      desc: "Line up is now up!",
+      hour: "10:35",
+    },
+    {
+      src: "./assets/team1.png",
+      title: "Shottery Mens First Team",
+      desc: "Line up is now up!",
+      hour: "10:35",
+    },
+  ];
+  return (
+    <>
+      <NotificationRow>
+        {notifications.map((notificaton) => (
+          <NotificationCard data={notificaton} />
+        ))}
+      </NotificationRow>
+    </>
+  );
+}
+
+export default AllNotifications;
