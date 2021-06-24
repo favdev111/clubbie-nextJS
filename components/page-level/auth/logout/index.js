1d956309b10dc7fdbfe57168257a5fbeca90f029
favdev111 <webdev0429@outlook.com>
2021-06-24 16:16:34 +0500

    chore: logout user

--- components/page-level/auth/logout/index.js
+++ components/page-level/auth/logout/index.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "@sub/button";
 import FacebookLogin from "@sub/button-facebook-auth/index";
 import GoogleLogin from "@sub/button-google-auth/index";
+import Auth from "@api/services/Auth";
+import authUser from "@utils/helpers/auth";
 import styles from "./logout.module.css";
 
 const Logout = () => {
+  useEffect(async () => {
+    await Auth.Logout({ refreshToken: authUser.getRefreshToken() })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    authUser.deleteUser();
+    authUser.deleteAccessToken();
+    authUser.deleteRefreshToken();
+  }, []);
+
   return (
     <div className={styles.logoutBlock}>
       <h1 className={styles.title}>Logged Out</h1>
