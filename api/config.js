f1351bdcbfff36b9137b3dcaeab132c98df260d6
favdev111 <webdev0429@outlook.com>
2021-06-28 14:08:47 +0500

    refactor: removed extra auth code

--- api/config.js
+++ api/config.js
@@ -1,14 +1,5 @@
 const API_BASE_URL = "https://dev-api.clubbie.com/v1/"; //process.env.API_BASE_URL;
 
-const SIGNUP_URL = `${API_BASE_URL}/v1/auth/signup`;
-const LOGIN_URL = `${API_BASE_URL}/v1/auth/login`;
-const LOGIN_GOOGLE_URL = `${API_BASE_URL}/v1/auth/login/google`;
-const VERIFY_ACCOUNT_URL = `${API_BASE_URL}/v1/auth/activate-account`;
+const LOGIN_GOOGLE_URL = `${API_BASE_URL}/auth/login/google`;
 
-export {
-  API_BASE_URL,
-  SIGNUP_URL,
-  LOGIN_URL,
-  LOGIN_GOOGLE_URL,
-  VERIFY_ACCOUNT_URL,
-};
+export { API_BASE_URL, LOGIN_GOOGLE_URL };
--- redux/auth.slice.js
+++ redux/auth.slice.js
@@ -1,24 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import {
-  signup as makeSignUpReq,
-  login as makeLoginReq,
-  googleLogin as makeGoogleLoginReq,
-  verifyAccount as makeVerifyAccountReq,
-} from "../api/auth.api";
-
-export const signup = createAsyncThunk(
-  "auth/signup",
-  async (credentials, store) => {
-    return makeSignUpReq(credentials);
-  }
-);
-
-export const login = createAsyncThunk(
-  "auth/login",
-  async (credentials, store) => {
-    return makeLoginReq(credentials);
-  }
-);
+import { googleLogin as makeGoogleLoginReq } from "../api/auth.api";
 
 export const googleLogin = createAsyncThunk(
   "auth/googleLogin",
@@ -27,50 +8,17 @@ export const googleLogin = createAsyncThunk(
   }
 );
 
-export const verifyAccount = createAsyncThunk(
-  "auth/verifyAccount",
-  async (credentials, store) => {
-    const token = store.getState().authReducer.tokens?.access?.token;
-    return makeVerifyAccountReq(token, credentials);
-  }
-);
-
-export const chatSlice = createSlice({
+export const authSlice = createSlice({
   name: "auth",
   initialState: {
     user: {},
     tokens: {},
     errors: {
-      loginError: null,
-      signupError: null,
-      verifyAccError: null,
       googleLoginError: null,
     },
   },
   reducers: {},
   extraReducers: {
-    [signup.pending]: (state) => {
-      state.errors.signupError = null;
-    },
-    [signup.fulfilled]: (state, { payload }) => {
-      state.user = payload.user;
-      state.tokens = payload.tokens;
-      state.errors.signupError = null;
-    },
-    [signup.rejected]: (state, { error }) => {
-      state.errors.signupError = error.message;
-    },
-    [login.pending]: (state) => {
-      state.errors.loginError = null;
-    },
-    [login.fulfilled]: (state, { payload }) => {
-      state.user = payload.user;
-      state.tokens = payload.tokens;
-      state.errors.loginError = null;
-    },
-    [login.rejected]: (state, { error }) => {
-      state.errors.loginError = error.message;
-    },
     [googleLogin.pending]: (state) => {
       state.errors.googleLoginError = null;
     },
@@ -82,17 +30,7 @@ export const chatSlice = createSlice({
     [googleLogin.rejected]: (state, { error }) => {
       state.errors.googleLoginError = error.message;
     },
-    [verifyAccount.pending]: (state) => {
-      state.errors.verifyAccError = null;
-    },
-    [verifyAccount.fulfilled]: (state, { payload }) => {
-      state.user = payload.user;
-      state.errors.verifyAccError = null;
-    },
-    [verifyAccount.rejected]: (state, { error }) => {
-      state.errors.verifyAccError = error.message;
-    },
   },
 });
 
-export default chatSlice.reducer;
+export default authSlice.reducer;
