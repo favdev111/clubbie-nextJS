788581f60632927949136b9e5216338a7f99686a
favdev111 <webdev0429@outlook.com>
2021-06-28 16:01:52 +0500

    chore: handle click outside of dropdown

--- components/sub/dropdown/index.js
+++ components/sub/dropdown/index.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Link from "next/link";
 import cn from "classnames";
 import styles from "./index.module.css";
 
+function useOutsideAlerter(ref, setOpen) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
+
 function DropDown({ Component, links }) {
   const [open, setOpen] = useState(false);
+
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, setOpen);
   /*
   links: [
     {
@@ -17,7 +35,7 @@ function DropDown({ Component, links }) {
   ]
   */
   return (
-    <div className={styles.dropDownWrapper}>
+    <div className={styles.dropDownWrapper} ref={wrapperRef}>
       <span onClick={() => setOpen(!open)}>
         <Component></Component>
       </span>
