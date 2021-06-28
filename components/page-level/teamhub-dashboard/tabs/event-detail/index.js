ed18ffd7af5b37b25e9d9c4a45eada5e9bff680e
favdev111 <webdev0429@outlook.com>
2021-06-28 22:43:34 +0300

    chore: event detail edit-confirm line-up-cancel event comp

--- components/page-level/teamhub-dashboard/tabs/event-detail/index.js
+++ components/page-level/teamhub-dashboard/tabs/event-detail/index.js
@@ -3,6 +3,11 @@ import styles from "./index.module.css";
 import DetailCover from "./cover";
 import MatchDetail from "./match-detail";
 import AvailablePlayers from "./players";
+import EditEvent from "@svg/edit-event";
+import ConfirmLineup from "@svg/confirm-lineup";
+import CancelEvent from "@svg/cancel-event";
+
+import RightArrow from "@svg/right-arrow";
 
 import Event from "@api/services/Event";
 
@@ -21,11 +26,29 @@ function EventDetail({ eventId, activeTeam, user }) {
 
   return (
     <div className={styles.eventDetail}>
-      <h1>Event Details</h1>
+      <h1>{data.title}</h1>
       <DetailCover img={data?.coverImage} />
       <div className={styles.twoRows}>
         <MatchDetail data={data} />
         {/*    <AvailablePlayers /> */}
+        <div className={styles.routeComponent}>
+          <div className={styles.center}>
+            <EditEvent /> Edit event
+          </div>
+          <RightArrow />
+        </div>
+        <div className={styles.routeComponent}>
+          <div className={styles.center}>
+            <ConfirmLineup /> Confirm Line-up
+          </div>
+          <RightArrow />
+        </div>
+        <div className={styles.routeComponent}>
+          <div className={styles.center}>
+            <CancelEvent /> Cancel event
+          </div>
+          <RightArrow />
+        </div>
       </div>
     </div>
   );
