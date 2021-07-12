fbb4a4add302e3f2098f52782495af0be626942c
favdev111 <webdev0429@outlook.com>
2021-07-12 13:57:14 +0500

    chore: tooltip component

---
+++ components/sub/tooltip/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useRef } from "react";
+import cn from "classnames";
+import styles from "./tooltip.module.css";
+
+function useOutsideAlerter(ref, setOpen) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setOpen(false);
+      } else {
+        setOpen(true);
+      }
+    }
+
+    document.addEventListener("mouseover", handleClickOutside);
+    return () => {
+      document.removeEventListener("mouseover", handleClickOutside);
+    };
+  }, [ref]);
+}
+
+function ToolTip({ text, children }) {
+  const [open, setOpen] = useState(false);
+
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, setOpen);
+
+  return (
+    <div className={styles.dropDownWrapper}>
+      <span ref={wrapperRef} className={styles.tipHover}>
+        {children || "?"}
+      </span>
+      <div className={cn(styles.dropDownList, open && styles.openDropdown)}>
+        {text || "ToolTip Text"}
+      </div>
+    </div>
+  );
+}
+
+export default ToolTip;
