8a1dd175742f4df132660a0805c319ff4e739a83
favdev111 <webdev0429@outlook.com>
2021-07-17 01:50:30 +0500

    chore: content dialog component

---
+++ components/sub/content-dialog/index.js
@@ -0,0 +1,63 @@
+import React, { useRef, useState, useEffect } from "react";
+import styles from "./index.module.css";
+import cn from "classnames";
+import Button from "@sub/button";
+
+function useOutsideAlerter(ref, setOpen) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
+
+function ContentDialog({
+  open,
+  setOpen,
+  title,
+  Body,
+  confirmText,
+  onConfirm,
+  onDismiss,
+  type,
+}) {
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, setOpen);
+
+  return (
+    <div className={cn(open ? styles.backdrop : styles.close)}>
+      <div className={styles.content} ref={wrapperRef}>
+        <h2>{title}</h2>
+        <div className={styles.body}>
+          <Body></Body>
+        </div>
+        <div className={styles.buttons}>
+          <Button
+            variant="cancel"
+            onClick={() => (onDismiss ? onDismiss() : setOpen(false))}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant={type || "danger"}
+            onClick={() => {
+              onConfirm();
+              setOpen(false);
+            }}
+          >
+            {confirmText}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContentDialog;
