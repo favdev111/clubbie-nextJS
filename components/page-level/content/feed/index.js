c86a68d9d8fdaba8fd326d65189ae4cf05125e0b
favdev111 <webdev0429@outlook.com>
2021-07-19 13:39:33 +0500

    chore: sports filters for posts

--- components/page-level/content/feed/index.js
+++ components/page-level/content/feed/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import styles from "./index.module.css";
 import CommonSearch from "@sub/search";
 import ConfirmDialog from "@sub/confirm-dialog";
 import useNotification from "@sub/hook-notification";
+import BackDropLoader from "@sub/backdrop-loader";
 import HomeVideosCard from "./card";
 import Tag from "./tag";
 import PlusTurk from "@svg/plus-turk";
@@ -12,6 +13,101 @@ import Loader from "@sub/loader";
 import InfiniteScroll from "@sub/infinite-scroll";
 import Posts from "@api/services/Posts";
 import sports from "@utils/fixedValues/sports";
+
+function PostFilters({ setFilter, fetchPosts, setLoading }) {
+  const [sportList] = useState([
+    ...new Set([
+      "All Sports",
+      "Football",
+      "Cricket",
+      "Netball",
+      "Boxing",
+      ...sports,
+    ]),
+  ]);
+  const [sportListExpanded, setSportListExpanded] = useState(false);
+  const [sportSelectedIndex, setSportSelectedIndex] = useState(0);
+  const [applyFilter, setApplyFilter] = useState(false);
+
+  const handleSportTagClick = (index) => {
+    setSportSelectedIndex(index);
+    setFilter((filter) => {
+      const _filter = { ...filter, search: sportList[index] };
+      if (sportList[index] === "All Sports") delete _filter["search"];
+      return _filter;
+    });
+    setApplyFilter(true);
+  };
+
+  useEffect(async () => {
+    if (applyFilter) {
+      setLoading(true);
+      await fetchPosts(1, true);
+      setApplyFilter(false);
+      setLoading(false);
+    }
+  }, [applyFilter]);
+
+  return (
+    <>
+      <div className={styles.mobileSort}>
+        <span> Recent </span>
+        <span> Following </span>
+        <span> Ranking </span>
+      </div>
+      <div className={styles.tagContent}>
+        <div className={styles.tagInner}>
+          {(sportListExpanded ? sportList : sportList.slice(0, 5)).map(
+            (tag, index) => (
+              <Tag
+                activeTag={sportSelectedIndex}
+                onClick={() => handleSportTagClick(index)}
+                index={index}
+                key={tag + index}
+              >
+                {tag}
+              </Tag>
+            )
+          )}
+          <Tag
+            sportSelectedIndex={false}
+            endingTag
+            onClick={() => setSportListExpanded(!sportListExpanded)}
+          >
+            {!sportListExpanded ? "More..." : "...Less"}
+          </Tag>
+        </div>
+
+        <div className={styles.sortSelect}>
+          <select name="sort" id="sort">
+            <option value="recent">Recent</option>
+            <option value="popular">Following</option>
+            <option value="Ranking">Ranking</option>
+          </select>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function AddContent() {
+  return (
+    <span>
+      <Link href="/content">
+        <a>
+          <div className={styles.addContent}>
+            <div className={styles.addButton}>
+              <a>
+                <PlusTurk />
+              </a>
+              Add Content
+            </div>
+          </div>
+        </a>
+      </Link>
+    </span>
+  );
+}
 
 function LoadingPosts() {
   return (
@@ -47,76 +143,18 @@ function Home({ posts, user }) {
   const createdPost = router?.query?.createdPost; // highlight a post if it was created
 
   const [_posts, setPosts] = useState(posts);
-  const [expandedSports, setExpandedSports] = useState(false);
-  const [displaySports, setDisplaySports] = useState([
-    ...new Set([
-      "All Sports",
-      "Football",
-      "Cricket",
-      "Netball",
-      "Boxing",
-      ...sports,
-    ]),
-  ]);
-  const [activeTag, setActiveTag] = useState(0);
   const [filter, setFilter] = useState({
     sortBy: "createdAt:desc",
   });
   const [showLoginPopup, setShowLoginPopup] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const { showNotificationMsg } = useNotification();
 
-  const getDefaultPosts = async () => {
+  const fetchPosts = async (page = _posts.page + 1, resetPosts = false) => {
     const response = await Posts.GetPosts({
       limit: 10,
-      page: 1,
-      ...filter,
-    }).catch(() => undefined);
-    const newPosts = response?.data;
-
-    if (!newPosts) {
-      console.log("Error fetching new posts"); // console for now
-      return;
-    }
-
-    const updatedPostState = {
-      ...newPosts,
-      results: [..._posts.results, ...newPosts.results],
-    };
-
-    setPosts({ ...updatedPostState });
-  };
-
-  const filterPosts = async (index) => {
-    setActiveTag(index)
-    if(displaySports[index] == 'All Sports') { 
-      getDefaultPosts()
-      return; 
-    }
-    
-    const response = await Posts.GetPosts({
-      limit: 100,
-      page: 1,
-      type: displaySports[index],
-      ...filter,
-    }).catch(() => undefined);
-    const newPosts = response?.data;
-
-    if (!newPosts) {
-      console.log("Error fetching new posts"); // console for now
-      return;
-    }
-
-    const updatedPostState = {
-      results: newPosts.results,
-    };
-    setPosts({ ...updatedPostState });
-  };
-
-  const fetchMorePosts = async () => {
-    const response = await Posts.GetPosts({
-      limit: 10,
-      page: _posts.page + 1,
+      page: page,
       ...filter,
     }).catch(() => undefined);
     const newPosts = response?.data;
@@ -129,16 +167,22 @@ function Home({ posts, user }) {
       return;
     }
 
+    // dump existing data on new filter
+    if (resetPosts) {
+      setPosts({ ...newPosts });
+      return;
+    }
+
     const updatedPostState = {
       ...newPosts,
       results: [..._posts.results, ...newPosts.results],
     };
-
     setPosts({ ...updatedPostState });
   };
 
   return (
     <div className={styles.homePage}>
+      {loading && <BackDropLoader />}
       <ConfirmDialog
         open={showLoginPopup}
         setOpen={setShowLoginPopup}
@@ -151,61 +195,15 @@ function Home({ posts, user }) {
         <CommonSearch />
       </div>
       <h1 className={styles.title}> Videos</h1>
-      <div className={styles.mobileSort}>
-        <span> Recent </span>
-        <span> Following </span>
-        <span> Ranking </span>
-      </div>
-      <div className={styles.tagContent}>
-        <div className={styles.tagInner}>
-          {(expandedSports ? displaySports : displaySports.slice(0, 5)).map(
-            (tag, index) => (
-              <Tag
-                activeTag={activeTag}
-                onClick={() => filterPosts(index)}
-                index={index}
-                key={tag + index}
-              >
-                {tag}
-              </Tag>
-            )
-          )}
-          <Tag
-            activeTag={false}
-            endingTag
-            onClick={() => setExpandedSports(!expandedSports)}
-          >
-            {!expandedSports ? "More..." : "...Less"}
-          </Tag>
-        </div>
-
-        <div className={styles.sortSelect}>
-          <select name="sort" id="sort">
-            <option value="recent">Recent</option>
-            <option value="popular">Following</option>
-            <option value="Ranking">Ranking</option>
-          </select>
-        </div>
-      </div>
-
-      <span>
-        <Link href="/content">
-          <a>
-            <div className={styles.addContent}>
-              <div className={styles.addButton}>
-                <a>
-                  <PlusTurk />
-                </a>
-                Add Content
-              </div>
-            </div>
-          </a>
-        </Link>
-      </span>
-
+      <PostFilters
+        setFilter={setFilter}
+        setLoading={setLoading}
+        fetchPosts={fetchPosts}
+      />
+      <AddContent />
       <InfiniteScroll
         dataLength={_posts?.totalResults}
-        getMore={fetchMorePosts}
+        getMore={fetchPosts}
         hasMore={_posts?.page < _posts?.totalPages}
         loader={<LoadingPosts />}
         endingMessage={<EndFeedMessage />}
