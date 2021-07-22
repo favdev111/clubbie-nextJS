2825d034f48bcc0b42f64f2791564175f86913c1
favdev111 <webdev0429@outlook.com>
2021-07-22 13:15:24 +0500

    refactor: profile posts css fix and repost removed

--- components/page-level/profile/posts/card/index.js
+++ components/page-level/profile/posts/card/index.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
+import cn from "classnames";
 import Link from "next/link";
 import styles from "./postCard.module.css";
 import FavSVG from "@svg/social/fav";
 import EyeSVG from "@svg/eye";
 import CommentSVG from "@svg/social/comment";
-import RepostSVG from "@svg/repost";
+// import RepostSVG from "@svg/repost";
 
 function ProfilePostCard({ post }) {
-  // TODO: add likes, comments, reposts, views count
   const [contentItem, setContentItem] = useState(
     post?.thumbnail || post?.media
   );
@@ -20,33 +20,33 @@ function ProfilePostCard({ post }) {
       <Link href={`/content/${post?.id}`}>
         <a>
           <div className={styles.profilePhotosItem}>
-            <span className={!loaded && styles.hidden}>
+            <span className={cn(styles.media, !loaded && styles.hidden)}>
               {contentItem?.includes("video") && (
                 <video src={contentItem}></video>
               )}
               {contentItem?.includes("image") && (
                 <img src={contentItem} onLoad={() => setLoaded(true)}></img>
               )}
+              <div className={styles.mediaCounts}>
+                <span>
+                  <FavSVG />
+                  {post?.counts?.likes}
+                </span>
+                <span>
+                  <EyeSVG filled />
+                  {post?.counts?.views}
+                </span>
+                <span>
+                  <CommentSVG />
+                  {post?.counts?.comments}
+                </span>
+                {/* <span>
+                  <RepostSVG filled />
+                  {post?.counts?.reposts}
+                </span> */}
+              </div>
             </span>
             <div className={!loaded && styles.loadingPost}></div>
-            <div className={styles.profilePhotosOptions}>
-              <span>
-                <FavSVG />
-                {post?.counts?.likes}
-              </span>
-              <span>
-                <EyeSVG filled />
-                {post?.counts?.views}
-              </span>
-              <span>
-                <CommentSVG />
-                {post?.counts?.comments}
-              </span>
-              <span>
-                <RepostSVG filled />
-                {post?.counts?.reposts}
-              </span>
-            </div>
           </div>
         </a>
       </Link>
