e36eff8c476d5d5992ae2a77ec29bd0d35b9b787
favdev111 <webdev0429@outlook.com>
2021-07-26 22:46:05 +0500

    chore: removed dynamic size fields from user obj while setting user cookies

--- utils/helpers/clientCookies.js
+++ utils/helpers/clientCookies.js
@@ -1,5 +1,20 @@
 import Cookie from "js-cookie";
 
+const config = {
+  userCookieConfig: (userObj) => {
+    const _userObj = { ...userObj };
+
+    // delete fields to not store in cookie
+    if (_userObj?.local?.password) delete _userObj.local.password;
+    if (_userObj?.clubs) delete _userObj.clubs;
+    if (_userObj?.teams) delete _userObj.teams;
+    if (_userObj?.stripe?.customer?.paymentMethods)
+      delete _userObj.stripe.customer.paymentMethods;
+
+    return _userObj;
+  },
+};
+
 const setCookie = (key, value, options = {}) => {
   return Cookie.set(key, value, options);
 };
@@ -13,6 +28,7 @@ const deleteCookie = (key) => {
 };
 
 export default {
+  config,
   setCookie,
   getCookie,
   deleteCookie,
