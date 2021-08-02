2f7a172555b409d707771e3719838db2dcd69a66
favdev111 <webdev0429@outlook.com>
2021-08-02 17:20:38 +0500

    chore: custom error messages added for add-subscription-plan form

--- utils/schemas/team.schema.js
+++ utils/schemas/team.schema.js
@@ -7,8 +7,12 @@ const editTeam = Joi.object().keys({
 });
 
 const addSubcriptionPlan = Joi.object().keys({
-  amount: Joi.string().required().messages({
-    "string.empty": "Amount is required",
+  amount: Joi.number().required().min(5000).max(10000).integer().messages({
+    "any.empty": "Amount is required",
+    "number.base": "Amount must be numeric",
+    "number.min": "Amount must be greater or equal to 5000",
+    "number.max": "Amount must be less or equal to 10000",
+    "number.integer": "Amount must not be decimal",
   }),
 });
 
