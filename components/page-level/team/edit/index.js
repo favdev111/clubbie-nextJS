5b9c6064877217cfc95540df00c9c9f4b5c234cf
favdev111 <webdev0429@outlook.com>
2021-08-02 14:57:51 +0500

    feat: team edit page connected with api + ux added

--- components/page-level/team/edit/index.js
+++ components/page-level/team/edit/index.js
@@ -1,58 +1,159 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import PhotoCamera from "@svg/edit-profile-pic";
 import Button from "@sub/button";
+import useForm from "@sub/hook-form";
 import TemplateInput from "@sub/input";
 import useNotification from "@sub/hook-notification";
 import styles from "./teamDetails.module.css";
+import Files from "@api/services/Files";
+import Teams from "@api/services/Teams";
+import { editTeam as editTeamSchema } from "@utils/schemas/team.schema";
 
-function TeamHeader({
-  clubId,
-  clubCrest,
-  clubName,
-  teamId,
-  teamCrest,
-  teamTitle,
-  showNotificationMsg,
-}) {
+function TeamHeader({ teamId, teamCrest, teamTitle, showNotificationMsg }) {
+  const [_teamCrest, setTeamCrest] = useState({
+    src: teamCrest || "/assets/club-badge-placeholder.png",
+    file: null,
+  });
+  const [_teamTitle, setTeamTitle] = useState(teamTitle);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+  const { register, handleSubmit, errors, setValue } = useForm({
+    schema: editTeamSchema,
+  });
+
+  useEffect(() => {
+    setValue("title", teamTitle);
+  }, [teamTitle]);
+
+  const onFileChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const imagePicked = {
+        src: e.target.result,
+        file,
+      };
+      setTeamCrest(imagePicked);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const onSubmit = async (data) => {
+    setLoading(true);
+
+    // upload team crest image if any
+    let teamCrestUploaded = null;
+    if (_teamCrest?.file) {
+      const imageForm = new FormData();
+      imageForm.append("files", _teamCrest.file);
+
+      const responseCrest = await Files.UploadFile("teamImg", imageForm, {
+        teamId,
+      }).catch(() => null);
+
+      teamCrestUploaded =
+        responseCrest?.data && responseCrest?.data[0]
+          ? responseCrest?.data[0]?.s3Url
+          : null;
+
+      if (!teamCrestUploaded) {
+        showNotificationMsg("Could Not Update Team", {
+          variant: "error",
+          displayIcon: true,
+        });
+        setLoading(false);
+        return;
+      }
+    }
+
+    const payload = { title: data?.title };
+    if (teamCrestUploaded) payload.crest = teamCrestUploaded;
+
+    const response = await Teams.UpdateTeam(teamId, payload).catch(() => null);
+    if (!response) {
+      showNotificationMsg("Could Not Update Team", {
+        variant: "error",
+        displayIcon: true,
+      });
+      setLoading(false);
+      return;
+    }
+
+    setLoading(false);
+    showNotificationMsg("Team Updated Successfully..!", {
+      variant: "success",
+      displayIcon: true,
+    });
+
+    router.push(`/teams/${teamId}`); //  redirect to team profile
+  };
+
   return (
     <>
-      <div className={styles.teamHeaderWrapper}>
-        <div>
-          <div className={styles.teamCrestWrapper}>
-            <img
-              className={styles.teamCrest}
-              src={teamCrest || "/assets/club-badge-placeholder.png"}
-            />
+      <form onSubmit={async (e) => await onSubmit(e)}>
+        <div className={styles.teamHeaderWrapper}>
+          <div>
+            <div className={styles.teamCrestWrapper}>
+              <img className={styles.teamCrest} src={_teamCrest?.src} />
+              <input
+                hidden
+                accept="image/*"
+                id="icon-button-file"
+                type="file"
+                onChange={onFileChange}
+              />
+              <label htmlFor="icon-button-file">
+                <span className={styles.teamClubCrestWrapper}>
+                  <PhotoCamera></PhotoCamera>
+                </span>
+              </label>
+            </div>
+          </div>
+          <div className={styles.teamHeaderDetailsWrapper}>
+            <div className={styles.teamHeaderTitleWrapper}>
+              <TemplateInput
+                placeholder="Team Title..."
+                size="large"
+                name="title"
+                value={_teamTitle}
+                onChange={(e) => setTeamTitle(e?.target?.value)}
+              ></TemplateInput>
+            </div>
+            <div className={styles.teamHeaderActionButtons}>
+              <Link href={`/teams/${teamId}`}>
+                <a>
+                  <Button variant="transparent" size="medium" type="button">
+                    Cancel
+                  </Button>
+                </a>
+              </Link>
+              <Button
+                variant="info"
+                size="medium"
+                loading={loading}
+                type="submit"
+              >
+                Save Changes
+              </Button>
+            </div>
           </div>
         </div>
-        <div className={styles.teamHeaderDetailsWrapper}>
-          <div className={styles.teamHeaderTitleWrapper}>
-            <TemplateInput value={teamTitle} size="large"></TemplateInput>
-          </div>
-          <div className={styles.teamHeaderActionButtons}>
-            <Button variant="transparent" size="medium">
-              Cancel
-            </Button>
-            <Button variant="info" size="medium">
-              Save Changes
-            </Button>
-          </div>
-        </div>
-      </div>
+      </form>
     </>
   );
 }
 
-function TeamDetails({ user, team }) {
-  const { showNotificationMsg } = useNotification();
+function TeamDetails({ team }) {
+  const [_team] = useState(team);
 
-  const [_team, setTeam] = useState(team);
+  const { showNotificationMsg } = useNotification();
 
   return (
     <TeamHeader
-      clubId={_team?.club?.id}
-      clubCrest={_team?.club?.crest}
-      clubName={_team?.club?.title}
       teamId={_team?.id}
       teamCrest={_team?.crest}
       teamTitle={_team?.title}
