180537fa7d08102fa0ca089057c3834b02511f5e
favdev111 <webdev0429@outlook.com>
2021-08-21 00:15:10 +0500

    chore: only allow team leaders or owners of event teams to edit event

--- pages/teamhub/events/[id]/edit.js
+++ pages/teamhub/events/[id]/edit.js
@@ -4,6 +4,7 @@ import Seo from "@layout/seo";
 import EditEvent from "@page/teamhub/main/events/edit";
 import Users from "@api/services/Users";
 import Events from "@api/services/Events";
+import roles from "@utils/fixedValues/rolesList";
 import { requiresPageAuth } from "@utils/middlewares/requiresPageAuth";
 
 function EditEventPage({ user, event }) {
@@ -26,7 +27,15 @@ export const getServerSideProps = requiresPageAuth(async (ctx) => {
   const responseEvent = await Events.GetEventById(eventId).catch(() => false);
   const _event = responseEvent?.data;
 
-  const notFound = !_user || !_event;
+  // validate if auth user can edit this event
+  const eventTeamIds = _event?.teams?.map((x) => x?.teamId?.id || x?.team?.id);
+  const isAuthorized = _user?.teams?.find(
+    (x) =>
+      (eventTeamIds?.includes(x?.team) && x?.role === roles.OWNER) ||
+      x?.role === roles?.TEAN_LEAD
+  );
+
+  const notFound = !_user || !_event || !isAuthorized;
 
   return {
     props: {
