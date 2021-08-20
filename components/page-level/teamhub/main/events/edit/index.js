a53f1ff0095be3f6180329e67f01e333d62395da
favdev111 <webdev0429@outlook.com>
2021-08-20 22:45:21 +0500

    chore: edit event page ui

--- components/page-level/teamhub/main/events/edit/index.js
+++ components/page-level/teamhub/main/events/edit/index.js
@@ -1,310 +1,98 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import React from "react";
+import cn from "classnames";
 import Link from "next/link";
-import { useForm } from "react-hook-form";
-import { DateTime } from "luxon";
-import { yupResolver } from "@hookform/resolvers/yup";
-import MessageToUser from "@sub/messageAnimation";
-import UploadMedia from "@sub/upload";
-import FormCell from "@sub/event-form-cell";
-import EventFormGrid from "@sub/event-form-grid";
-import Event from "@api/services/Events";
-import Files from "@api/services/Files";
-import Teams from "@api/services/Teams";
-import { schema } from "@utils/schemas/editEvent";
+import TemplateInput from "@sub/input";
+import Button from "@sub/button";
+import DragDrop from "@sub/drag-drop";
+import UploadSVG from "@svg/upload";
 import styles from "./index.module.css";
-import FormSubmit from "./submit";
 
-/* Todo -> Display validate errors */
-
-function EditEvent({ user, activeTeam }) {
-  const [responseMessage, setResponseMessage] = useState();
-  const [isError, setError] = useState(false);
-  const [isSuccess, setSuccess] = useState(false);
-  const [eventData, setData] = useState(null);
-  const [userTeams, setUserTeams] = useState([]);
-  const [media, setMedia] = useState(null);
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-  const router = useRouter();
-
-  /* Media */
-  const onFileChange = (e) => {
-    const file = e.target.files[0];
-    if (!file) return;
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      const mediaPicked = {
-        src: e.target.result,
-        file,
-      };
-      setMedia(mediaPicked);
-    };
-    reader.readAsDataURL(file);
-  };
-
-  const deleteMedia = () => {
-    setMedia(null);
-  };
-
-  /* Fetch teams for selectbox */
-  useEffect(() => {
-    const fetchUserTeams = async () => {
-      /* queries */
-      const arr = [];
-      user.teams.map((i) => {
-        arr.push(i.team);
-      });
-      const queries = arr.reduce((a, b) => {
-        let sum = `${a}` + `&id=${b}`;
-        return sum;
-      });
-
-      const response = await Teams.GetTeamsWithDetail(`id=${queries}`);
-      const allUserTeams = response.data;
-      setUserTeams(allUserTeams);
-    };
-
-    fetchUserTeams();
-  }, []);
-
-  /* Fetch current values for form */
-  useEffect(() => {
-    const fetchPromise = new Promise((resolve, reject) => {
-      const eventData = async () => {
-        const response = await Event.FetchSingleEvent(
-          router.query.eventId,
-          activeTeam
-        );
-        return response;
-      };
-      eventData().then((response) => {
-        const data = response.data;
-        setData(data);
-      });
-      resolve(eventData());
-    });
-
-    fetchPromise
-      .then((res) => {
-        const dateValue = DateTime.fromISO(res.data.eventDateTime, {
-          zone: "utc",
-        });
-        const date =
-          `${dateValue.year}` +
-          "-" +
-          `${n(dateValue.month)}` +
-          "-" +
-          `${n(dateValue.day)}`;
-
-        const values = {
-          title: res.data?.title,
-          location: res.data?.location,
-          message: res.data?.message,
-          fee: res.data?.fee,
-          eventDate: date,
-        };
-        return values;
-      })
-      .then((values) => {
-        setTimeout(() => {
-          reset(values);
-        }, 1000);
-      });
-
-    return;
-  }, [reset]);
-
-  /* Submit */
-  const onSubmit = async (data) => {
-    let mediaIdToUpload = null;
-    if (media?.src && media?.file) {
-      const mediaForm = new FormData();
-      mediaForm.append("files", media?.file);
-      await Files.UploadFile(
-        media?.src?.includes("image") && "resourceFile",
-        mediaForm
-      )
-        .then((res) => {
-          mediaIdToUpload = res?.data[0]?.s3Url;
-        })
-        .catch((err) => {
-          alert(err?.response?.data?.message); // TODO: error comp
-        });
-    }
-
-    const eventDateTime =
-      data?.eventDate + "T" + data?.eventDateTime + ":00.000Z";
-
-    const formBody = {
-      title: data?.title,
-      location: data?.location,
-      eventDateTime: eventDateTime,
-      message: data?.message,
-      fee: data?.fee || null,
-      coverImage: mediaIdToUpload,
-    };
-    const updateBody = Object.fromEntries(
-      Object.entries(formBody).filter(([_, v]) => v != null)
-    );
-
-    await Event.EditEventbyId(router.query.eventId, updateBody)
-      .then((res) => {
-        setResponseMessage("Succesfully changed.");
-        setSuccess(true);
-        setTimeout(() => {
-          router.push(`/teamhub/events/${res.data.id}`);
-        }, 3000);
-      })
-      .catch((err) => {
-        setResponseMessage(err.response.data.message);
-        setError(true);
-      });
-  };
-
-  /* Get min date */
-  function n(n) {
-    return n > 9 ? "" + n : "0" + n;
-  }
-  const dateIso = DateTime.fromISO(eventData?.eventDateTime, { zone: "utc" });
-  const minDate =
-    `${dateIso.year}` + "-" + `${n(dateIso.month)}` + "-" + `${n(dateIso.day)}`;
-
+function EditEventForm() {
   return (
-    <div className={styles.addEvent}>
-      {/* Header */}
-      <div className={styles.header}>
-        <h1> Edit Event</h1>
-        <Link href={`/teamhub/events/${router.query.eventId}`}>
-          <a>Cancel</a>
+    <form>
+      <div className={styles.formGridWrapper}>
+        <div className={cn(styles.span2, styles.gridItem)}>
+          <TemplateInput
+            type="text"
+            name="title"
+            placeholder="Edit Title"
+            inputClassName={styles.editEventTitleInput}
+          />
+        </div>
+        <div className={cn(styles.span1, styles.gridItem)}>
+          <p>When?</p>
+          <TemplateInput type="date" name="date" />
+        </div>
+        <div className={cn(styles.span1, styles.gridItem)}>
+          <p>What Time?</p>
+          <TemplateInput type="time" name="time" />
+        </div>
+        <div className={cn(styles.span1, styles.gridItem)}>
+          <p>Where?</p>
+          <TemplateInput
+            type="text"
+            name="location"
+            placeholder="Edit Match Location"
+          />
+        </div>
+        <div className={cn(styles.span1, styles.gridItem)}>
+          <p>Add Fee?</p>
+          <TemplateInput type="number" name="fee" placeholder="Fee" />
+        </div>
+        <div className={cn(styles.span1, styles.gridItem)}>
+          <p>Add Message?</p>
+          <TemplateInput
+            type="text"
+            name="message"
+            placeholder="Message for Invitees"
+          />
+        </div>
+        <div className={cn(styles.span2, styles.gridItem)}>
+          <p>Cover Image</p>
+          <DragDrop className={cn(styles.dragDropVideos)}>
+            <input
+              hidden
+              name="media"
+              accept="image/*"
+              id="pick-parent-media"
+              type="file"
+            />
+            <label htmlFor="pick-parent-media">
+              <UploadSVG></UploadSVG>
+            </label>
+            <span className={styles.marginTop}>
+              <span>Drag and drop an Image or</span>
+              &ensp;
+              <a className={styles.dragDropVideosBrowseFiles}>
+                <label htmlFor="pick-parent-media">Browse Files</label>
+              </a>
+            </span>
+          </DragDrop>
+        </div>
+        <div className={cn(styles.span2, styles.gridItem)}>
+          <div className={styles.submitButtonWrapper}>
+            <Button type="submit">Edit</Button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+function EditEvent({ user, event }) {
+  return (
+    <div className={styles.editEventWrapper}>
+      <div className={styles.editEventHeader}>
+        <h1 className={styles.editEventTitle}>Edit Event</h1>
+        <Link href="/teamhub/event">
+          <a>
+            <span>Cancel</span>
+          </a>
         </Link>
       </div>
-
-      {/* Body */}
-      <div className={styles.content}>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-          <input
-            className={styles.formTitle}
-            type="text"
-            placeholder={eventData?.title}
-            {...register("title", { required: true, maxLength: 20 })}
-          />
-          <p> {errors.title?.message} </p>
-          <div className={styles.eventType} /> {/*  Space */}
-          <EventFormGrid>
-            <FormCell>
-              Team A
-              <select
-                disabled
-                {...register("teamA", { required: true })}
-                className={styles.inputStyle}
-              >
-                {userTeams.map((i) => (
-                  <option value={i.id} key={`${i}12 +${Math.random()}`}>
-                    {i.title}
-                  </option>
-                ))}
-              </select>
-            </FormCell>
-            <FormCell>
-              Team B
-              <select
-                disabled
-                {...register("teamB", { required: true })}
-                className={styles.inputStyle}
-              >
-                <option value="60d371268ffc8b40e175fb4b">Team 2</option>
-              </select>
-            </FormCell>
-            <FormCell>
-              When?
-              <input
-                className={styles.inputStyle}
-                type="date"
-                min={minDate}
-                required
-                {...register("eventDate", { required: true })}
-              />
-            </FormCell>
-            <FormCell>
-              What time?
-              <input
-                className={styles.inputStyle}
-                type="time"
-                required
-                {...register("eventDateTime", { required: true })}
-              />
-            </FormCell>
-            <FormCell>
-              Who?
-              <select className={styles.inputStyle}>
-                <option {...register("createdBy")}>1</option>
-              </select>
-            </FormCell>
-            <FormCell>
-              Where?
-              <input
-                className={styles.inputStyle}
-                type="text"
-                placeholder={eventData?.location}
-                required
-                {...register("location", { required: true })}
-              />
-            </FormCell>
-            <FormCell>
-              Add Fee?
-              <input
-                className={styles.inputStyle}
-                min="5000"
-                type="number"
-                {...register("fee")}
-              />
-            </FormCell>
-            <FormCell>
-              Add Message?
-              <input
-                className={styles.inputStyle}
-                type="text"
-                placeholder={eventData?.message}
-                {...register("message")}
-              />
-            </FormCell>
-            <FormCell>
-              Cost Of Event
-              <input
-                className={styles.inputStyle}
-                type="number"
-                {...register("cost")}
-              />
-            </FormCell>
-          </EventFormGrid>
-          <FormCell>
-            Change Cover Image
-            <UploadMedia
-              onFileChange={onFileChange}
-              deleteMedia={deleteMedia}
-              media={media}
-            />
-          </FormCell>
-          <FormSubmit>
-            <button type="submit" className={styles.button}>
-              Post
-            </button>
-          </FormSubmit>
-        </form>
+      <div className={styles.editEventBodyWrapper}>
+        <EditEventForm user={user} event={event} />
       </div>
-      {/* Form success/error messages */}
-      {isError && <MessageToUser message={responseMessage} err={isError} />}
-
-      {isSuccess && (
-        <MessageToUser message={responseMessage} err={!isSuccess} />
-      )}
     </div>
   );
 }
