95175b1e43aba8b4e31a469bb51336aad0d3b4e0
favdev111 <webdev0429@outlook.com>
2021-08-25 22:07:44 +0500

    chore: matchPositions value list added

---
+++ utils/fixedValues/matchPositions.js
@@ -0,0 +1,38 @@
+/* 
+  GK: goal keeper
+  Numbers: left -> right (column-wise)
+  Alphabets: top -> bottom (row-wise)
+*/
+module.exports = Object.freeze([
+  'A1',
+  'A2',
+  'A3',
+  'A4',
+  'A5',
+
+  'B1',
+  'B2',
+  'B3',
+  'B4',
+  'B5',
+
+  'C1',
+  'C2',
+  'C3',
+  'C4',
+  'C5',
+
+  'D1',
+  'D2',
+  'D3',
+  'D4',
+  'D5',
+
+  'E1',
+  'E2',
+  'E3',
+  'E4',
+  'E5',
+
+  'GK',
+]);
