0ec216990d3b1df0394d3bbc703035ecce667600
favdev111 <webdev0429@outlook.com>
2021-08-25 21:44:50 +0500

    chore: select mode added to match event formation list

--- components/page-level/teamhub/main/events/lineup/common/formationList/index.js
+++ components/page-level/teamhub/main/events/lineup/common/formationList/index.js
@@ -3,10 +3,12 @@ import cn from "classnames";
 import matchFormations from "@utils/fixedValues/matchFormations";
 import styles from "./index.module.css";
 
-function FormationList({ selected, onFormationSet }) {
+function FormationList({ selected, onFormationSet, selectMode }) {
+  const [_selectMode, setSelectMode] = useState(false);
   const [_selected, setSelected] = useState(matchFormations["433"]);
 
   useEffect(() => {
+    selectMode && setSelectMode(selectMode);
     selected && setSelected(selected);
   }, []);
 
@@ -20,12 +22,19 @@ function FormationList({ selected, onFormationSet }) {
   };
 
   return (
-    <ul className={cn(styles.formationList)}>
+    <ul
+      className={cn(
+        styles.formationList,
+        !selectMode && styles.formationListCenter
+      )}
+    >
       {Object.values(matchFormations)?.map((x) => {
+        if (!selectMode && x !== _selected) return <></>;
         return (
           <li
             className={cn(
               styles.formationListItem,
+              selectMode && styles.formationListItemHover,
               x === _selected && styles.activeFormation
             )}
             onClick={() => handleFormationClick(x)}
