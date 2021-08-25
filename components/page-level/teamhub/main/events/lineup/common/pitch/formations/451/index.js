538757f13c65d802affadbeb764b1909690630c8
favdev111 <webdev0429@outlook.com>
2021-08-25 22:05:11 +0500

    chore: 4-5-1 formation component built on top of formations base comp.

---
+++ components/page-level/teamhub/main/events/lineup/common/pitch/formations/451/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import FormationBase from "../base";
+import styles from "./index.module.css";
+
+const Formation451 = ({
+  editMode,
+  playerShirtColor,
+  goalKeeperShirtColor,
+  lineup,
+}) => {
+  const _defaultLineup = [
+    { position: "A1" },
+
+    { position: "B1" },
+    { position: "B2" },
+    { position: "B3" },
+    { position: "B4" },
+    { position: "B5" },
+
+    { position: "C1" },
+    { position: "C2" },
+    { position: "C3" },
+    { position: "C4" },
+
+    { position: "GK" },
+  ];
+
+  return (
+    <FormationBase
+      editMode={editMode}
+      playerShirtColor={playerShirtColor}
+      goalKeeperShirtColor={goalKeeperShirtColor}
+      defaultLineup={_defaultLineup}
+      lineup={lineup}
+      styleSheet={styles}
+    />
+  );
+};
+
+export default Formation451;
