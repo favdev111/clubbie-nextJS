3ca15425981154ed8561863eef3bc07b4fa0d7d9
favdev111 <webdev0429@outlook.com>
2021-08-26 15:35:54 +0500

    chore: next Link comp added to teanhub nav

--- components/page-level/teamhub/main/navigation/index.js
+++ components/page-level/teamhub/main/navigation/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import cn from "classnames";
 import styles from "./index.module.css";
@@ -7,14 +8,24 @@ function TeamhubNav() {
   const router = useRouter();
 
   const navItems = [
-    { title: "Dashboard", active: router?.pathname === "/teamhub" },
-    { title: "Events", active: router?.pathname?.includes("/teamhub/events") },
+    {
+      title: "Dashboard",
+      link: "/teamhub",
+      active: router?.pathname === "/teamhub",
+    },
+    {
+      title: "Events",
+      link: "/teamhub/events",
+      active: router?.pathname?.includes("/teamhub/events"),
+    },
     {
       title: "Payments",
+      link: "/teamhub/payments",
       active: router?.pathname?.includes("/teamhub/payments"),
     },
     {
       title: "Statistics",
+      link: "/teamhub/statistics",
       active: router?.pathname?.includes("/teamhub/statistics"),
     },
   ];
@@ -23,18 +34,16 @@ function TeamhubNav() {
     <div className={styles.teamhubNav}>
       <ul className={styles.navList}>
         {navItems.map((item) => (
-          <li
-            className={cn(styles.navItem, item?.active && styles.selected)}
-            onClick={() => {
-              index == 0 && router.push("/teamhub");
-              index == 1 && router.push("/teamhub/events");
-              index == 2 && router.push("/teamhub/payments");
-              index == 3 && router.push("/teamhub/statistics");
-            }}
-            key={item?.title}
-          >
-            {item?.title}
-          </li>
+          <Link href={item?.link}>
+            <a>
+              <li
+                className={cn(styles.navItem, item?.active && styles.selected)}
+                key={item?.title}
+              >
+                {item?.title}
+              </li>
+            </a>
+          </Link>
         ))}
       </ul>
     </div>
